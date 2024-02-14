3535087189d6872c0e7370ab9fd6777911eadbdb
Paula <psanzilundain@gmail.com>
2024-02-14 13:45:31 +0100

    Variables Javascript

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Bea - Preview
+const namePreview = document.querySelector('.js-name-preview');
+const jobPreview = document.querySelector('.js-job-preview');
+const imagePreview = document.querySelector('.js-image-preview');
+const phonePreview = document.querySelector('.js-phone-preview');
+const mailPreview = document.querySelector('.js-mail-preview');
+const linkedinPreview = document.querySelector('.js-linkedin-preview');
+const githubPreview = document.querySelector('.js-github-preview');
+
+//Noelia - Input
+const nameInput = document.querySelector('.js-name');
+const jobInput = document.querySelector('.js-job');
+const imageInput = document.querySelector('.js-image');
+const phoneInput = document.querySelector('.js-phone');
+const mailInput = document.querySelector('.js-mail');
+const linkedinInput = document.querySelector('.js-linkedin');
+const githubInput = document.querySelector('.js-github');
+
+//Desplegables
+const headerDesign = document.querySelector('js-design');
+const headerFill = document.querySelector('js-fill');
+const headerShare = document.querySelector('js-share');
+
+//Containers
+const containerDesign = document.querySelector('js-design-container');
+const containerFill = document.querySelector('js-fill-container');
+const containerShare = document.querySelector('js-share-container');
