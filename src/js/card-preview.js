0ffffbd818ab726a3c1df0dc7d5ae976f829608a
Paula <psanzilundain@gmail.com>
2024-03-11 12:07:47 +0100

    add class hidden

--- src/js/card-preview.js
+++ src/js/card-preview.js
@@ -6,10 +6,10 @@ const renderCardPreview = () => {
   linkedinPreview.href = data.linkedin;
   githubPreview.href = data.github;
   imagePreview.style.backgroundImage = `url('${data.photo}')`;
-  if (data.palette === "2") {
-    card.classList.add("palette2");
-  } else if (data.palette === "3") {
-    card.classList.add("palette3");
+  if (data.palette === '2') {
+    card.classList.add('palette2');
+  } else if (data.palette === '3') {
+    card.classList.add('palette3');
   }
 };
 
@@ -18,30 +18,31 @@ if (localStorageData !== null) {
   renderCardPreview();
 } else {
   data = {
-    palette: "1",
-    name: "",
-    job: "",
-    phone: "",
-    email: "",
-    linkedin: "",
-    github: "",
-    photo: "",
+    palette: '1',
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+    photo: '',
   };
 }
 
 function handleReset() {
   mainForm.reset();
-  namePreview.innerHTML = "Nombre Apellido";
-  jobPreview.innerHTML = "Front-end developer";
-  mailPreview.href = "";
-  phonePreview.href = "";
-  linkedinPreview.href = "";
-  githubPreview.href = "";
+  namePreview.innerHTML = 'Nombre Apellido';
+  jobPreview.innerHTML = 'Front-end developer';
+  mailPreview.href = '';
+  phonePreview.href = '';
+  linkedinPreview.href = '';
+  githubPreview.href = '';
   imagePreview.style.backgroundImage =
     "url('https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos.jpg')";
   thumbnail.style.backgroundImage = "url('')";
-  card.classList.remove("palette2", "palette3");
-  localStorage.removeItem("localStorageData");
+  card.classList.remove('palette2', 'palette3');
+  twitterSection.classList.add('hidden');
+  localStorage.removeItem('localStorageData');
 }
 
-formReset.addEventListener("click", handleReset);
+formReset.addEventListener('click', handleReset);
